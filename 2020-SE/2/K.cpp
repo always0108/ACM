8b1290795eb31599cf9b1eb56c3200a3ac59dd3d
李猛 <ldx19980108@gmail.com>
2020-10-09 21:22:20 +0800

    减少循环

--- 2020-SE/2/K.cpp
+++ 2020-SE/2/K.cpp
@@ -12,10 +12,18 @@ int main(void)
 {
     int m, n, i, j;
     cin >> m >> n;
-    for(i = 1; i <= m; i++){
+    /*for(i = 1; i <= m; i++){
         for(j = i + 1; j <= m; j++){
             if(i*i + n*n == j*j){
                 cout << n << " " << i << " " << j << endl;
+            }
+        }
+    }*/
+    //优化后
+    for(i = n + 1; i <= m; i++){
+        for(j = 3; j < i; j++){ //直角边为1、2时，斜边都不是整数
+            if(i*i == j*j + n*n){
+                cout << n << " " << j << " " << i << endl;
             }
         }
     }
