d2464167192f06d62362da23299215293d355bec
李猛 <ldx19980108@gmail.com>
2020-10-23 22:59:26 +0800

    修改代码

--- 2020-SE/3/L.cpp
+++ 2020-SE/3/L.cpp
@@ -7,108 +7,77 @@
 
 /*输入4个正整数，不改变他们的顺序，问你只使用加减乘除能否得到24.*/
 /*没有好的思路，暂时没有解决*/
+/*答案一半对，一半错*/
 #include<iostream>
-#include <cstdlib>
-#include <sstream>
-#include<string>
 #include<stack>
 using namespace std;
-int a,b,c,d;
 char op[4] = {'+','-','*','/'};
+double num[4];
+char a[3];
+int get_pre(char op){
+	if(op == '+')
+		return 0; 
+	if(op == '-')
+		return 0;
+	if(op == '/')
+		return 1; 
+	if(op == '*')
+  		return 2;
+}
 
-
-void expression_generation()
+double cal(double x, double y, char op)
 {
-    int i,j,k;
-    for(i = 0; i < 4; i++)
-        for(j = 0; j < 4; j++)
-            for(k = 0; k < 4; k++){
-                ostringstream oss;
-                oss << a << op[i] << b << op[j] << c << op[k] << d; 
-                cout << oss.str() << endl;
-            }
+	if(op == '+')
+		return x + y; 
+	if(op == '-')
+  		return x - y; 
+	if(op == '*')
+  		return x * y; 
+	if(op == '/')
+  		return x / y; 
+}
+
+double fun(char op[])
+{
+	stack<double> s1;
+ 	s1.push(num[0]);
+ 	s1.push(num[1]);
+ 	stack<char> s2;
+ 	s2.push(op[0]);
+ 	int i = 1, j = 2;
+ 	while(!s2.empty()){
+  		char tmp = s2.top();
+  		if(i < 3 && get_pre(op[i]) >= get_pre(tmp)){
+   			s1.push(num[j++]);
+   			s2.push(op[i++]); 
+  		}else{
+   			int a = s1.top();
+   			s1.pop();
+   			int b = s1.top();
+   			s1.pop();
+   			s2.pop();
+   			s1.push(cal(a,b,tmp));
+  		}
+	}
+ 	return s1.top();
 }
 
 int main(void)
 {
-    cin >> a >> b >> c >> d;
-    expression_generation();
-    return 0;
-}
-
-int priority(char ch){ //优先级 
-	 if (ch == '(') return 1;  
-    else if (ch == '+' || ch == '-') return 2;  
-    else if (ch == '*' || ch == '/') return 3;  
-    else return 4;
-}
-
-string midpresent(string str){ //转后缀
-	stack<char> s;
-	int len=str.length(), i=0;
-	char temp;
-	string res="";
-	for(int i=0;i<len;i++){
-		if(str[i] == ' ') continue;
-        if(str[i] >='0' && str[i] <='9') res.push_back(str[i]);
-		else if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/'){
-			if(s.empty()) s.push(str[i]);
-			else {
-				while(!s.empty()){
-					temp = s.top();
-					if(priority(temp) >= priority(str[i])){
-						res.push_back(temp);
-						s.pop();
-					}
-					else break;
-				}
-				s.push(str[i]);
-			}
-		}
-		else {
-			if(str[i] == '(') s.push(str[i]);
-			else{
-				while(s.top() != '('){
-					res.push_back(s.top());
-					s.pop();
-				}
-				s.pop();
-			}
-		}
-	}
-	while(!s.empty()){
-		res.push_back(s.top());
-		s.pop();
-	}
-	return res;
-}
-
-double cal(string str){ //计算后缀
-	int len=str.length();
-	stack<double> s;
-	double num1,num2;
-	for(int i=0;i<len;i++){
-		if(str[i] >='0' && str[i] <='9'){
-		     s.push(str[i] - '0');
-		}
-		else{
-			num2=s.top();
-			s.pop();
-			num1=s.top();
-			s.pop();
-			if(str[i] == '+'){
-				s.push(num1+num2);
-			}
-			else if(str[i] == '-'){
-				s.push(num1-num2);
-			}
-			if(str[i] == '*'){
-				s.push(num1*num2);
-			}
-			if(str[i] == '/'){
-				s.push(num1/num2);
-			}
-		}
-	}
-	return s.top();
+	int i,j,k;
+ 	for(i = 0; i < 4; i++)
+  		cin >> num[i];
+ 	for(i = 0; i < 4; i++)
+  		for(j = 0; j < 4; j++)
+   			for(k = 0; k < 4;k++){
+    			a[0] = op[i];
+    			a[1] = op[j];
+    			a[2] = op[k];
+    			if(fun(a) == 24){
+     				cout << "Yes" << endl;
+     			return 0;
+    			}
+   			}
+	cout << "No" << endl;
+	return 0;
 }
